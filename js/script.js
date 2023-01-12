10341b1f2c97552bf19323c3c0ef5a952d007203
Mohd. Rahil <112772865+mohdrahil101@users.noreply.github.com>
2023-01-12 19:38:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 var menu=document.querySelector(".menu");
 var navlist=document.querySelector(".navlist");
-var link=document.querySelector(".navlist li");
+var navbar=document.querySelector(".navbar");
 
 menu.addEventListener("click",function(){
     this.classList.toggle("click");
     navlist.classList.toggle("clicked");
+    navbar.classList.toggle('h-nav');
+    navlist.classList.toggle("v-nav");
+
 });
 
 function check(e){
     menu.classList.toggle("click");
     navlist.classList.toggle("clicked");
 }
+
